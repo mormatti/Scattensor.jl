299b35136c6d03fa886632b03b6385d5d0900d47
Mattia Morgavi <mormatti@gmail.com>
2025-09-11 16:50:25 +0200

    From N generic to only N = 3 in glueballs

--- glueballs/0_constants.jl
+++ glueballs/0_constants.jl
@@ -12,157 +12,111 @@ using JLD2
 using Measures
 using LaTeXStrings
 
-# Local operators
-function ketbra(m::Integer, n::Integer, q::Integer)
-    M = zeros(Int64, q, q)
-    M[m, n] = 1
-    return SparseMatrixCSC(M)
-end
-
-# Valid for both Z2 and SU(3)
-function lambda_from_g(g::Real)
-    return g^4 / (1 + g^4)
-end
-
-function g_from_lambda(λ::Real)
-    if λ < 0 || λ >= 1
-        error("Invalid λ value")
+let
+    # Local operators
+    function ketbra(m::Integer, n::Integer, q::Integer)
+        M = zeros(Int64, q, q)
+        M[m, n] = 1
+        return SparseMatrixCSC(M)
     end
-    return (λ/(1 - λ))^(1/4)
-end
 
+    # Valid for both Z2 and SU(3)
+    function lambda_from_g(g::Real)
+        return g^4 / (1 + g^4)
+    end
 
-locops = (JLD2.load("glueballs/locops.jld2"))["single_stored_object"]
+    function g_from_lambda(λ::Real)
+        if λ < 0 || λ >= 1
+            error("Invalid λ value")
+        end
+        return (λ/(1 - λ))^(1/4)
+    end
 
-Nlist = [2,3]
-locops["ZZ"] = Dict()
-locops["SU"] = Dict()
-ZZ = locops["ZZ"]
-SU = locops["SU"]
+    locops = (JLD2.load("glueballs/0_constants.jld2"))["single_stored_object"]
 
-for N in Nlist
-    ZZ[N] = Dict()
-    SU[N] = Dict()
+    locops["ZZ3"] = Dict()
+    locops["SU3"] = Dict()
+    ZZ3 = locops["ZZ3"]
+    SU3 = locops["SU3"]
 
     # Local dimension
-    ZZ[N]["N"] = N
-    SU[N]["N"] = N
-    ZZ[N]["localdim"] = N
-    SU[N]["localdim"] = N
+    ZZ3["localdim"] = 3
+    SU3["localdim"] = 3
 
     # Identity matrix
-    idm = operator_identity(SparseMatrixCSC, N)
-    ZZ[N]["I1"] = idm
-    ZZ[N]["I2"] = kron(idm, idm)
-    ZZ[N]["I3"] = kron(idm, idm, idm)
-    SU[N]["I1"] = idm
-    SU[N]["I2"] = kron(idm, idm)
-    SU[N]["I3"] = kron(idm, idm, idm)
+    idm = operator_identity(SparseMatrixCSC, 3)
+    ZZ3["I1"] = idm
+    ZZ3["I2"] = kron(idm, idm)
+    ZZ3["I3"] = kron(idm, idm, idm)
+    SU3["I1"] = idm
+    SU3["I2"] = kron(idm, idm)
+    SU3["I3"] = kron(idm, idm, idm)
 
     # Local pure reflection
-    ZZ[N]["R2"] = operator_reflection(SparseMatrixCSC, N, 2)
-    ZZ[N]["R3"] = operator_reflection(SparseMatrixCSC, N, 3)
-    SU[N]["R2"] = operator_reflection(SparseMatrixCSC, N, 2)
-    SU[N]["R3"] = operator_reflection(SparseMatrixCSC, N, 3)
+    ZZ3["R2"] = operator_reflection(SparseMatrixCSC, 3, 2)
+    ZZ3["R3"] = operator_reflection(SparseMatrixCSC, 3, 3)
+    SU3["R2"] = operator_reflection(SparseMatrixCSC, 3, 2)
+    SU3["R3"] = operator_reflection(SparseMatrixCSC, 3, 3)
 
     # The cyclic supradiagonal local operator
-    tau = spzeros(N, N)
-    for i in 1:N-1
-        tau[i, i+1] = 1
-    end
-    tau[N, 1] = 1
-    ZZ[N]["tau"] = tau
-    SU[N]["tau"] = tau
+    tau = spzeros(3, 3)
+    tau[1, 2] = 1
+    tau[2, 3] = 1
+    tau[3, 1] = 1
+    ZZ3["tau"] = tau
+    SU3["tau"] = tau
 
     # The charge conjugation 1,2,3-local operator
-    cc = spzeros(N, N)
-    for i in 1:N
-        cc[1 + mod(-(i-1), 0:N-1), i] = 1
-    end
-    ZZ[N]["C1"] = cc
-    ZZ[N]["C2"] = kron(cc, cc)
-    ZZ[N]["C3"] = kron(cc, cc, cc)
-    SU[N]["C1"] = cc
-    SU[N]["C2"] = kron(cc, cc)
-    SU[N]["C3"] = kron(cc, cc, cc)
+    cc = spzeros(3, 3)
+    cc[1, 1] = 1
+    cc[2, 3] = 1
+    cc[3, 2] = 1
+    ZZ3["C1"] = cc
+    ZZ3["C2"] = kron(cc, cc)
+    ZZ3["C3"] = kron(cc, cc, cc)
+    SU3["C1"] = cc
+    SU3["C2"] = kron(cc, cc)
+    SU3["C3"] = kron(cc, cc, cc)
 
-    kb(n,m) = ketbra(n,m,N)
+    kb(n,m) = ketbra(n,m,3)
 
     # Electric field (casimir) as a 2-local operator
-    C2ZZ(N,n) = N^2 / π^2 * sin(π * n / N)^2
-    C2SU(N,n) = (N + 1) // (2N) * (N - n) * n
-    casimirZZN = spzeros(N^2, N^2)
-    casimirSUN = spzeros(N^2, N^2)
-    for n1 in 0:N-1
-        for n2 in 0:N-1
+    C2ZZ3(n) = 3^2 / π^2 * sin(π * n / 3)^2
+    C2SU3(n) = 2 // 3 * (3 - n) * n
+    casimirZZ3 = spzeros(3^2, 3^2)
+    casimirSU3 = spzeros(3^2, 3^2)
+    for n1 in 0:2
+        for n2 in 0:2
             i1 = n1 + 1
             i2 = n2 + 1
-            m = mod(n1 - n2, 0:N-1)
-            E2ZZN = C2ZZ(N,n1) + C2ZZ(N,n2) + C2ZZ(N,m)
-            E2SUN = C2SU(N,n1) + C2SU(N,n2) + C2SU(N,m)
-            casimirZZN += E2ZZN * kron(kb(i1,i1), kb(i2,i2))
-            casimirSUN += E2SUN * kron(kb(i1,i1), kb(i2,i2))
+            m = mod(n1 - n2, 0:2)
+            E2ZZ3 = C2ZZ3(n1) + C2ZZ3(n2) + C2ZZ3(m)
+            E2SU3 = C2SU3(n1) + C2SU3(n2) + C2SU3(m)
+            casimirZZ3 += E2ZZ3 * kron(kb(i1,i1), kb(i2,i2))
+            casimirSU3 += E2SU3 * kron(kb(i1,i1), kb(i2,i2))
         end
     end
-    ZZ[N]["Esq2"] = casimirZZN
-    SU[N]["Esq2"] = casimirSUN
+    ZZ3["Esq2"] = casimirZZ3
+    SU3["Esq2"] = casimirSU3
 
     # Electric field (casimir) as a 3-local operator
-    ZZ[N]["Esq3"] = 1/2 * kron(ZZ[N]["Esq2"], ZZ[N]["I1"]) + 1/2 * kron(ZZ[N]["I1"], ZZ[N]["Esq2"])
-    SU[N]["Esq3"] = 1/2 * kron(SU[N]["Esq2"], SU[N]["I1"]) + 1/2 * kron(SU[N]["I1"], SU[N]["Esq2"])
+    ZZ3["Esq3"] = 1/2 * kron(ZZ3["Esq2"], ZZ3["I1"]) + 1/2 * kron(ZZ3["I1"], ZZ3["Esq2"])
+    SU3["Esq3"] = 1/2 * kron(SU3["Esq2"], SU3["I1"]) + 1/2 * kron(SU3["I1"], SU3["Esq2"])
 
     # Plaquette term for ZN
-    ZZ[N]["Up"] = ZZ[N]["tau"]
-    ZZ[N]["Up3"] = kron(ZZ[N]["I1"], ZZ[N]["tau"], ZZ[N]["I1"])
+    ZZ3["Up"] = ZZ3["tau"]
+    ZZ3["Up3"] = kron(ZZ3["I1"], ZZ3["tau"], ZZ3["I1"])
 
     # Diagonal D terms for SU(N)
-    DN = []
-    if N == 2
-        append!(DN, [Diagonal([1, -1/2])])
-        append!(DN, [Diagonal([1, -1/2])])
-    elseif N == 3
-        append!(DN, [Diagonal([1, -1/√3, 1/3])])
-        append!(DN, [Diagonal([1, 1/3, -1/√3])])
-        append!(DN, [Diagonal([1, -1/√3, -1/√3])])
-    elseif N == 4
-        error("N = 4 is not implemented yet")
-        append!(DN, [Diagonal([1, 1, 1, 1])])
-        append!(DN, [Diagonal([1, 1, 1, 1])])
-        append!(DN, [Diagonal([1, 1, 1, 1])])
-        append!(DN, [Diagonal([1, 1, 1, 1])])
-    elseif N == 5
-        error("N = 5 is not implemented yet")
-        append!(DN, [Diagonal([1, 1, 1, 1, 1])])
-        append!(DN, [Diagonal([1, 1, 1, 1, 1])])
-        append!(DN, [Diagonal([1, 1, 1, 1, 1])])
-        append!(DN, [Diagonal([1, 1, 1, 1, 1])])
-        append!(DN, [Diagonal([1, 1, 1, 1, 1])])
-    elseif N == 6
-        error("N = 6 is not implemented yet")
-        append!(DN, [Diagonal([1, 1, 1, 1, 1, 1])])
-        append!(DN, [Diagonal([1, 1, 1, 1, 1, 1])])
-        append!(DN, [Diagonal([1, 1, 1, 1, 1, 1])])
-        append!(DN, [Diagonal([1, 1, 1, 1, 1, 1])])
-        append!(DN, [Diagonal([1, 1, 1, 1, 1, 1])])
-        append!(DN, [Diagonal([1, 1, 1, 1, 1, 1])])
-    elseif N == 7
-        error("N = 7 is not implemented yet")
-        append!(DN, [Diagonal([1, 1, 1, 1, 1, 1, 1])])
-        append!(DN, [Diagonal([1, 1, 1, 1, 1, 1, 1])])
-        append!(DN, [Diagonal([1, 1, 1, 1, 1, 1, 1])])
-        append!(DN, [Diagonal([1, 1, 1, 1, 1, 1, 1])])
-        append!(DN, [Diagonal([1, 1, 1, 1, 1, 1, 1])])
-        append!(DN, [Diagonal([1, 1, 1, 1, 1, 1, 1])])
-        append!(DN, [Diagonal([1, 1, 1, 1, 1, 1, 1])])
-    end
+    Dg1 = Diagonal([1, 1/√3, 1/3])
+    Dg2 = Diagonal([1, 1/3, 1/√3])
+    Dg3 = Diagonal([1, 1/√3, 1/√3])
 
     # Plaquette term for ZN
-    Up = kron(DN[1], kb(N,1), DN[1])
-    for n in 2:N
-        Up += kron(DN[n], kb(n-1,n), DN[n])
-    end
-    SU[N]["Up"] = Up
-    SU[N]["Up3"] = Up
-end
+    Up = kron(Dg1, kb(3,1), Dg1) + kron(Dg2, kb(1,2), Dg2) + kron(Dg3, kb(2,3), Dg3)
 
-JLD2.save_object("glueballs/locops.jld2", locops)
+    SU3["Up"] = Up
+    SU3["Up3"] = Up
+
+    JLD2.save_object("glueballs/0_constants.jld2", locops)
+end
--- glueballs/1_smallsize.jl
+++ glueballs/1_smallsize.jl
@@ -1,38 +1,34 @@
-locops = (JLD2.load("glueballs/locops.jld2"))["single_stored_object"]
-smallsize = (JLD2.load("glueballs/smallsize.jld2"))["single_stored_object"]
+locops = (JLD2.load("glueballs/0_constants.jld2"))["single_stored_object"]
+smallsize = (JLD2.load("glueballs/1_smallsize.jld2"))["single_stored_object"]
 # smallsize = Dict()
 
-groups = ["ZZ", "SU"]
-Nlist = [2,3]
+groups = ["ZZ3", "SU3"]
+lengths = [10, 11]
 
 # Compute disprel
-if false
+if true
     for group in groups
     smallsize[group] = Dict()
 
-    for N in Nlist
-    smallsize[group][N] = Dict()
-
-    d = locops[group][N]["localdim"]
+    d = 3
     # Coupling loop
     λ_list = [1//10, 3//10, 5//10, 7//10, 9//10]
 
     for λ ∈ λ_list
-    smallsize[group][N][λ] = Dict()
+    smallsize[group][λ] = Dict()
 
     # Small system size loop
-    ls = [8]
-    for l ∈ ls
-    smallsize[group][N][λ][l] = Dict()
-    dct = smallsize[group][N][λ][l]
+    for l ∈ lengths
+    smallsize[group][λ][l] = Dict()
+    dct = smallsize[group][λ][l]
 
     # Local operators
-    dct["h"] = λ * locops[group][N]["Esq3"] - (1 - λ) * (locops[group][N]["Up3"] + locops[group][N]["Up3"]')
+    dct["h"] = λ * locops[group]["Esq3"] - (1 - λ) * (locops[group]["Up3"] + locops[group]["Up3"]')
 
     # Operators
-    dct["C"] = kron_power(SparseMatrixCSC(locops[group][N]["C1"]), l)
+    dct["C"] = kron_power(SparseMatrixCSC(locops[group]["C1"]), l)
     dct["T"] = operator_translation(SparseMatrixCSC, d, l)
-    dct["P"] = operator_reflection(SparseMatrixCSC, d, l)
+    dct["P"] = operator_reflection(SparseMatrixCSC, d, l) # For this model is like that
     dct["H"] = summation_local(dct["h"], d, l; pbc = true)
     dct["R"] = dct["P"] * dct["C"]
 
@@ -55,7 +51,7 @@ if false
     for el in firstband
         if el.koverpi == 0//1 || el.koverpi == 1//1 || el.koverpi == -1//1
             push!(energiesband, energy(el))
-        else
+        else # We count two times the others
             push!(energiesband, energy(el))
             push!(energiesband, energy(el))
         end
@@ -64,11 +60,10 @@ if false
 
     end # system size loop
     end # coupling loop
-    end # N loop
     end # group loop
 end
 
-JLD2.save_object("glueballs/smallsize.jld2", smallsize)
+JLD2.save_object("glueballs/1_smallsize.jld2", smallsize)
 
 function color_from_lambda(λ::Real)
     pE = [0.2,0.3,0.7]
@@ -79,84 +74,83 @@ end
 
 # plot disprel
 if true
-    l = 8
+    l = 10
     λ_list = [1//10, 3//10, 5//10, 7//10, 9//10]
     ncols = length(λ_list)
-    for group in ["ZZ", "SU"]
-        for N in [2,3]
-            plt = Plots.plot(
-                layout=(1,ncols), 
-                legend = false,
-                plot_padding = 0mm,
-                margin = 0mm,
-                framestyle = :box,
-                guidefont = font(7),     # axis labels
-                tickfont  = font(7),     # tick labels
-                legendfont = font(7),    # legend
-                titlefont = font(7)
-                )
-            for ii in 1:ncols
-                λ = λ_list[ii]
-                Egs = smallsize[group][N][λ][l]["gsenergy"]
-                Ebar = smallsize[group][N][λ][l]["centroid"]
-                ΔE = Ebar - Egs
-                Elist = smallsize[group][N][λ][l]["disprel"]["E"]
-                Klist = smallsize[group][N][λ][l]["disprel"]["k"]
-                Clist = smallsize[group][N][λ][l]["disprel"]["C"]
-                neg_C = findall(c -> c < 0, Clist)
-                pos_C = findall(c -> c > 0, Clist)
+    for group in ["ZZ3", "SU3"]
+        plt = Plots.plot(
+            layout=(1,ncols), 
+            legend = false,
+            plot_padding = 0mm,
+            margin = 0mm,
+            framestyle = :box,
+            guidefont = font(7),     # axis labels
+            tickfont  = font(7),     # tick labels
+            legendfont = font(7),    # legend
+            titlefont = font(7)
+            )
+        for ii in 1:ncols
+            λ = λ_list[ii]
+            Egs = smallsize[group][λ][l]["gsenergy"]
+            Ebar = smallsize[group][λ][l]["centroid"]
+            ΔE = Ebar - Egs
+            Elist = smallsize[group][λ][l]["disprel"]["E"]
+            Klist = smallsize[group][λ][l]["disprel"]["k"]
+            Clist = smallsize[group][λ][l]["disprel"]["C"]
+            neg_C = findall(c -> c < 0, Clist)
+            pos_C = findall(c -> c > 0, Clist)
 
+            leb = L"\langle\mathcal{E}\rangle"
+            ytickstoput = ([Egs, Egs + ΔE, Egs + 2ΔE, Egs + 3ΔE], [L"0", leb, L"2" * leb, L"3" * leb])
+            leftmargin = 0mm
+            if ii != 1
+                ytickstoput = ([Egs, Egs + ΔE, Egs + 2ΔE, Egs + 3ΔE], ["", "", "", ""])
+                leftmargin = -5mm
+            end
+
+            if group == "SU3"
                 leb = L"\langle\mathcal{E}\rangle"
-                ytickstoput = ([Egs, Egs + ΔE, Egs + 2ΔE, Egs + 3ΔE], [L"0", leb, L"2" * leb, L"3" * leb])
+                ytickstoput = ([Egs, Egs + ΔE, Egs + 2ΔE], [L"0", leb, L"2" * leb])
                 leftmargin = 0mm
                 if ii != 1
-                    ytickstoput = ([Egs, Egs + ΔE, Egs + 2ΔE, Egs + 3ΔE], ["", "", "", ""])
+                    ytickstoput = ([Egs, Egs + ΔE, Egs + 2ΔE], ["", "", ""])
                     leftmargin = -5mm
                 end
-
-                if group == "SU"
-                    leb = L"\langle\mathcal{E}\rangle"
-                    ytickstoput = ([Egs, Egs + ΔE, Egs + 2ΔE], [L"0", leb, L"2" * leb])
-                    leftmargin = 0mm
-                    if ii != 1
-                        ytickstoput = ([Egs, Egs + ΔE, Egs + 2ΔE], ["", "", ""])
-                        leftmargin = -5mm
-                    end
-                end
-
-                titlelatex = L"\lambda = \dfrac{%$(numerator(λ))}{%$(denominator(λ))}"
-
-                multipleformargin = 3
-                if group == "SU"
-                    multipleformargin = 2.5
-                end
-
-                Plots.scatter!(
-                    plt, 
-                    Klist[pos_C], 
-                    Elist[pos_C], 
-                    color = :blue, 
-                    subplot = ii, 
-                    markersize = 2, 
-                    xlims  = [0-0.3, π+0.3],
-                    xticks = ([0, π/2, π], [L"0", L"\dfrac{\pi}{2}", L"\pi"]),
-                    ylims  = [Egs - 0.05 * (3ΔE), Egs + multipleformargin * ΔE + 0.05 * (3ΔE)],
-                    yticks = ytickstoput,
-                    left_margin = leftmargin,
-                    title = titlelatex
-                    )
-                Plots.scatter!(
-                    plt,
-                    Klist[neg_C], 
-                    Elist[neg_C], 
-                    color = :red, 
-                    marker = :diamond, 
-                    markersize = 2, 
-                    subplot = ii
-                    )
             end
-            Plots.plot!(size=(220,180), grid=true)
-            Plots.savefig("Disprel$(group)$(N).pdf")
+
+            titlelatex = L"\lambda = \dfrac{%$(numerator(λ))}{%$(denominator(λ))}"
+
+            multipleformargin = 3
+            if group == "SU3"
+                multipleformargin = 2.5
+            end
+
+            Plots.scatter!(
+                plt, 
+                Klist[pos_C], 
+                Elist[pos_C], 
+                color = :blue, 
+                subplot = ii, 
+                markersize = 2, 
+                xlims  = [0-0.3, π+0.3],
+                xticks = ([0, π/2, π], [L"0", L"\dfrac{\pi}{2}", L"\pi"]),
+                ylims  = [Egs - 0.05 * (3ΔE), Egs + multipleformargin * ΔE + 0.05 * (3ΔE)],
+                yticks = ytickstoput,
+                left_margin = leftmargin,
+                title = titlelatex
+                )
+            Plots.scatter!(
+                plt,
+                Klist[neg_C], 
+                Elist[neg_C], 
+                color = :red, 
+                marker = :diamond, 
+                markersize = 2, 
+                subplot = ii
+                )
+                print("Size of plt: ", size(Klist[pos_C]))
         end
+        Plots.plot!(size=(220,180), grid=true)
+        Plots.savefig("disprel$(group).pdf")
     end
 end
